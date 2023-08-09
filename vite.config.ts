1e8ec86e0c847748a60f212a07c697013ce4c424
gdsav <124171344+gdsav@users.noreply.github.com>
2023-08-09 14:20:02 -0400

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, ConfigEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: { 
+    hmr: {
+      host: 'localhost',
+      port: 3177,
+  },
+  }
+})
+
+
+
